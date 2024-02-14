04b3dbeb08321f0a790337fc85fe47dc0319585b
José Daniel Moreno Sánchez <jdmoreno98@hotmail.com>
2024-02-14 11:58:41 +0100

    Cambios realizados para basarnos en módulos. Así vamos eliminando morralla en el app.module para ir modularizando nuestra app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './counter/components/counter/counter.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
     ListComponent,
-    CounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [
     provideClientHydration()
