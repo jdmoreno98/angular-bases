bb74b2d41af0c1e8ba58d1b2dd210ab53e683152
José Daniel Moreno Sánchez <jdmoreno98@hotmail.com>
2024-02-15 10:19:40 +0100

    @Input - Recibir del padre. Explicacion en el list.ts

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  /*
+   * Este characterList lo vamos a utilizar en este propio componente list.component.html
+   * Podría llamarse exactamente igual que en la propiedad definida en el main-page.component.ts porque el scope de la variable es por clase
+   ! Lo que necesitamos es que desde el padre (MainPage ya que es quien contiene el <app-dbz-list>) nos llegue el listado de personajes a esta clase.
+   ! Para permitir esta comunicación, la propiedad que deseemos recibir va a tener el decorador @Input.
+   ! Lo que significa esto es que este componente  va o PUEDE recibir una propiedad llamada (en este caso ) characterList
+   * Para cambiarle el nombre de la propiedad @Input('nombre a usar en el mundo exterior')
+   ! Tras esto, si nos vamos al main-page.html podemos observar que utilizando [nombrePropiedadConElDecoradorInput] podemos seleccionar la propiedad definida en el padre
+    * <app-dbz-list [characterList]="characters"></app-dbz-list>
+   */
 
+   @Input()
+  public characterList : Character[] = [{
+    name:'Trunks',
+    power: 2
+  }];
 }
