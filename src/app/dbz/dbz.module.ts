db7acf3d0c4ec1db9892a6fef5464a3b81b45b8f
José Daniel Moreno Sánchez <jdmoreno98@hotmail.com>
2024-02-15 09:58:17 +0100

    Creación del componente de añadir personaje y de la lista de estos.

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-
+import { ListComponent } from './components/list/list.component';
+import { AddComponent } from './components/add/add.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddComponent,
   ],
   exports:[MainPageComponent],
   imports: [
